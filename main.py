9f3b8e4430726c3c7a241b1943f4947100cc9e4f
Damian Sztyber <damian.sztyber@pwc.com>
2024-02-01 11:58:46 +0100

    Added main file

---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request, Depends
+
+app = FastAPI(title="Library")
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', reload=True)
